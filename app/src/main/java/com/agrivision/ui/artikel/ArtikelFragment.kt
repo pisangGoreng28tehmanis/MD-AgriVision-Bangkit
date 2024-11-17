8c4396c5656519e5268babf64954862fda388f27
explorenewthingwithme <149031657+explorenewthingwithme@users.noreply.github.com>
2024-11-17 14:11:56 +0700

    Artikel Item Layout
    Menampilkan layout sedikit dari artikel fragment, dengan dummy data dan array strings dan drawable

--- app/src/main/java/com/agrivision/ui/artikel/ArtikelFragment.kt
+++ app/src/main/java/com/agrivision/ui/artikel/ArtikelFragment.kt
@@ -4,22 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.agrivision.R
+import com.agrivision.data.News
 import com.agrivision.databinding.FragmentArtikelBinding
 
 class ArtikelFragment : Fragment() {
 
     private var _binding: FragmentArtikelBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var rvNews: RecyclerView
+
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val artikelViewModel =
@@ -28,11 +30,32 @@ class ArtikelFragment : Fragment() {
         _binding = FragmentArtikelBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        artikelViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        rvNews = binding.rvNews  // Initialize RecyclerView from the binding
+        rvNews.setHasFixedSize(true)
+
+        // Show the list of articles
+        showRecyclerList()
+
         return root
+    }
+
+    private fun getListHeroes(): ArrayList<News> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+        val listHero = ArrayList<News>()
+        for (i in dataName.indices) {
+            val hero = News(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+            listHero.add(hero)  // Fixed incorrect variable name from 'news' to 'hero'
+        }
+        return listHero
+    }
+
+    private fun showRecyclerList() {
+        val list = getListHeroes()  // Get the list of heroes
+        rvNews.layoutManager = LinearLayoutManager(context)
+        val listHeroAdapter = ListNewsAdapter(list)
+        rvNews.adapter = listHeroAdapter
     }
 
     override fun onDestroyView() {
